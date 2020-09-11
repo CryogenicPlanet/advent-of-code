0127ac9db05d3b08f598213ada8e986b36c63f8d
Rahul Tarak <rahul.tarakbalaji@mail.utoronto.ca>
2020-09-11 20:11:47 +0000

    Solved Day 1

--- 2019/day1/sol.py
+++ 2019/day1/sol.py
@@ -1,1 +1,25 @@
+import math
 
+def determineFuel(num):
+    fuel =  math.floor(num / 3)
+    fuel -= 2
+    return fuel
+
+def calcFuel(originalMass):
+    curMass = originalMass
+    fuelPerMass = 0
+    curFuel = 0
+    while curMass > 0:
+        fuelPerMass += curFuel
+        curFuel = determineFuel(curMass)
+        curMass = curFuel
+        # print('Fuel Iteraion', curFuel)
+        
+    return fuelPerMass
+
+inputFile = open("input.txt", "r")
+# inputFile = [1969]
+totalSum = 0
+for line in inputFile:
+  totalSum += calcFuel(int(line))
+print(totalSum)
